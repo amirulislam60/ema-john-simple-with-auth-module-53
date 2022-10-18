a9969c2f5d1df83ff67d991d17477677475c1cbf
amirulislam <amirulislam5181@gmail.com>
2022-10-18 23:16:35 +0600

    first commit

---
+++ src/-auth.steps.jsx
@@ -0,0 +1,8 @@
+/*
+1.AUTH SETUP
+2.Create firebase project
+3.enable sign in method
+4.install firebase
+5.get firebase config in your firebase.config.js file
+6.export app from firebase.config.js
+*/ 
---
+++ src/Firebase/Firebase.jsx
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDJSLyFyZfYSFvpWWVYRbQHUZrvXhRSN4c",
+    authDomain: "ema-john-simple-router-a-a5128.firebaseapp.com",
+    projectId: "ema-john-simple-router-a-a5128",
+    storageBucket: "ema-john-simple-router-a-a5128.appspot.com",
+    messagingSenderId: "738960136717",
+    appId: "1:738960136717:web:bad2273c052a363ae0d344"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
